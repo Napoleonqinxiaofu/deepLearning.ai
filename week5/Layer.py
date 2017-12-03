3f5fa928da4581688cc5481ea7a1a71d0c6e50f3
Qin Xiaofu <boxingdream@sina.com>
2017-12-03 21:03:46 +0800

    split the net into single layer

--- week5/Layer.py
+++ week5/Layer.py
@@ -7,8 +7,27 @@ import numpy as np
 
 class Layer(object):
 
-    def __init__(self):
+    def __init__(self, units, activation="relu", L2=True, dropout=False):
+        """
+
+        :param units {tuple}: units is like (prev_layer_unit, current_layer_units)
+        :param activation {String}: activation function name
+        :param L2 {Boolean}: use L2 regularization or not.
+        :param dropout {Boolean}: use dropout or not
+        """
+        (prev, current) = units
+        # Initialize the weights and biases
+        self.weights = np.random.randn(prev, current) * np.sqrt(1 / prev)
+
+    def back_propagation(self):
         pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        return self.train()
+
 
 if __name__ == "__main__":
     pass
---
+++ week5/activation.py
@@ -0,0 +1,7 @@
+# fileName: activation
+# author: xiaofu.qin
+# create at 2017/12/3
+# description:
+
+if __name__ == "__main__":
+    pass
