4fa5873d177f4fb23e36e28231de386a73da7a53
Qin Xiaofu <boxingdream@sina.com>
2017-12-03 17:36:54 +0800

    Delete the mnist handwriting dataset.

---
+++ week5/Layer.py
@@ -0,0 +1,14 @@
+# fileName: Layer
+# author: xiaofu.qin
+# create at 2017/12/3
+# description: single layer class
+import numpy as np
+
+
+class Layer(object):
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
